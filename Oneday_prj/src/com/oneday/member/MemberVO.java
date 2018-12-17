f2998514dd29a84c0b4284b944e43aea3b192c29
KDH2 <KDH2@DESKTOP-7F6PI5O>
2018-12-17 19:19:20 +0900

    membervo 추가

---
+++ Oneday_prj/src/com/oneday/member/MemberVO.java
@@ -0,0 +1,76 @@
+package com.oneday.member;
+
+public class MemberVO {
+
+	private int m_seq;
+	private String m_email;
+	private String m_pw;
+	private String m_name;
+	private String m_nick;
+	private String m_resnum;
+	private String m_gubun;
+	private String m_regdate;
+	private String m_del;
+	private String m_deldate;
+	
+	public int getM_seq() {
+		return m_seq;
+	}
+	public void setM_seq(int m_seq) {
+		this.m_seq = m_seq;
+	}
+	public String getM_email() {
+		return m_email;
+	}
+	public void setM_email(String m_email) {
+		this.m_email = m_email;
+	}
+	public String getM_pw() {
+		return m_pw;
+	}
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
+	}
+	public String getM_name() {
+		return m_name;
+	}
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+	public String getM_nick() {
+		return m_nick;
+	}
+	public void setM_nick(String m_nick) {
+		this.m_nick = m_nick;
+	}
+	public String getM_resnum() {
+		return m_resnum;
+	}
+	public void setM_resnum(String m_resnum) {
+		this.m_resnum = m_resnum;
+	}
+	public String getM_gubun() {
+		return m_gubun;
+	}
+	public void setM_gubun(String m_gubun) {
+		this.m_gubun = m_gubun;
+	}
+	public String getM_regdate() {
+		return m_regdate;
+	}
+	public void setM_regdate(String m_regdate) {
+		this.m_regdate = m_regdate;
+	}
+	public String getM_del() {
+		return m_del;
+	}
+	public void setM_del(String m_del) {
+		this.m_del = m_del;
+	}
+	public String getM_deldate() {
+		return m_deldate;
+	}
+	public void setM_deldate(String m_deldate) {
+		this.m_deldate = m_deldate;
+	}
+}
